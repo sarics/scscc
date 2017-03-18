44faea9e7690d4aa4d799c83e1bab12e0d08cbe9
sarics <sari.csaba@outlook.hu>
2017-03-18 14:49:53 +0100

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    background: './background_scripts/index.js',
+    content: './content_scripts/index.js',
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
