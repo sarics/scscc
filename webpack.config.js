52e669f4b90d4ff8425e884e071546bad9720ae3
sarics <sari.csaba@outlook.hu>
2017-03-19 15:03:54 +0100

    remove dist dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   context: path.resolve(__dirname, 'src'),
@@ -40,4 +41,12 @@ module.exports = {
       },
     ],
   },
+
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: 'manifest.json',
+      },
+    ]),
+  ],
 };
